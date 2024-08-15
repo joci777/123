83b78fc933f7f270a76d039c8ccc45264d0cd190
Estudande alura <104458433+joci777@users.noreply.github.com>
2024-08-15 11:01:40 -0300

    Add files via upload

---
+++ alura obre i.ajoci/I.A/script.js
@@ -0,0 +1,3 @@
+const caixaPrincipal = document.querySlector(".caixa-principal");
+const caixaPergunta = document.querySlector(".caixa-perguntas");
+const caixaAlternativas = document.querySlector(".caixa-alternativas");
